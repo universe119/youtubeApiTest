48c612b0abd6a54afb425d0b8f8319d1dd9d89f5
universe119 <ruddh119@gmail.com>
2024-09-24 11:28:35 +0900

    json형식 데이터추가

--- youtube.js
+++ youtube.js
@@ -1,1 +1,7 @@
+// // 서버에서 "실시간으로" 외부 데이터를 내 웹 페이지 문서로 가져오는 방법
+// fetch("요청URL").then(data=>data.json()).then(json=>{
+//   // 요청한 서버쪽으로부터 성공적으로 데이터를 전달받으면 json이라는 매개변수 데이터 확인 가능
+//   console.log(json);
+// })
 console.log("youtube");
+const api_key = "AIzaSyC0YTkwnKAxe7Th6bkOdlmS5uW4auLXs8s";
